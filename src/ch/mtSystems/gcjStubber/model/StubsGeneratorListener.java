8ca1c395cacc9f30de8421674d24d18838f94a1f
soapy <soapy@37e54391-8d16-0410-88f2-9af82bd9c324>
2007-07-07 09:17:03 +0000

    Added phase statistics

--- src/ch/mtSystems/gcjStubber/model/StubsGeneratorListener.java
+++ src/ch/mtSystems/gcjStubber/model/StubsGeneratorListener.java
@@ -21,7 +21,29 @@ package ch.mtSystems.gcjStubber.model;
 
 public interface StubsGeneratorListener
 {
-	public void log(String line);
-	public void progress(int cur, int total);
+	public void started();
+	public void actionDone(String msg);
+
+	/**
+	 * Notifies about an complete phase for an object.
+	 *  
+	 * @param objectName The name of the processed object.
+	 * @param phaseProcessed The phase:
+	 *                       - 0: Skipped (No Java Classes)
+	 *                       - 1: Skipped (Won't be pulled in)
+	 *                       - 2: Phase 1
+	 *                       - 3: Phase 2
+	 *                       - 4: Phase 3
+	 * @param phaseResult The result of the phase:
+	 *                    - 0: Ok
+	 *                    - 1: Failed
+	 *                    - 2: Problem
+	 * @param phaseResultMsg If the phase result is 1 or 2, this contains a problem description.
+	 * @param objectIndex The index of the object (1 ... totalCount)
+	 * @param totalCount The total count of all objects.
+	 */
+	public void processed(String objectName, int phaseProcessed, int phaseResult,
+			String phaseResultMsg, int objectIndex, int totalCount);
+	
 	public void done();
 }
